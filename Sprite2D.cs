0567e0d920b6b7e3f7aecb688223c5f02214364e
Iain Sandison <iain.sandison.mousa@gmail.com>
2023-04-01 13:23:57 +0100

    make icon move in a circle, removed scale

--- Sprite2D.cs
+++ Sprite2D.cs
@@ -9,7 +9,13 @@ public partial class Sprite2D : Godot.Sprite2D
 
     public override void _Process(double delta) {
         Rotation += AngularSpeed * (float)delta;
-        this.ApplyScale(new Vector2((float)1.1, (float)1.1));
+
+        // velocity has both direction AND speed (it's a vector of the two)
+        // The UP.rotated(rotation) is a vector which points ahead RELATIVE to the icon
+        var velocity = Vector2.Up.Rotated(Rotation) * Speed;
+
+        Position += velocity * (float)delta;
+        // this.ApplyScale(new Vector2((float)1.1, (float)1.1));
     }
 
 }
